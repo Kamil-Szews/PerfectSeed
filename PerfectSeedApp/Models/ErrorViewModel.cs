58ce80e949697c43af2d533e74b90daa2d204077
Kamil Szews <kamilszews.it@gmail.com>
2023-11-02 23:07:38 +0100

    Add project files.

---
+++ PerfectSeedApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PerfectSeedApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
