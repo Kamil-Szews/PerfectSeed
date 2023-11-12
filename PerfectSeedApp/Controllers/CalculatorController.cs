8d06c7858bdf8f5f9208dd791280caffa7034ec7
Kamil Szews <kamilszews.it@gmail.com>
2023-11-12 22:28:55 +0100

    Trash commit

--- PerfectSeedApp/Controllers/CalculatorController.cs
+++ PerfectSeedApp/Controllers/CalculatorController.cs
@@ -8,13 +8,23 @@ namespace PerfectSeedApp.Controllers
 {
     public class CalculatorController : Controller
     {
+        #region Fields and Properties
+
         private readonly DataBaseContext _db;
         private IList<Calculator> _calculators;
-        
+
+        #endregion
+
+        #region Constructors
+
         public CalculatorController(DataBaseContext db)
         {
             _db = db;
         }
+
+        #endregion
+
+        #region Index
 
         //GET
         public IActionResult Index()
@@ -26,30 +36,46 @@ namespace PerfectSeedApp.Controllers
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Index(Calculator obj)
+        public IActionResult Index(Calculator? obj)
         {
+            if(obj.Id == null || obj.Seed == null)
+            {
+                return RedirectToAction("Index");
+            }
+
             _db.Calculator.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
 
+        #endregion
+
+        #region Calculate
+
         public IActionResult Calculate()
         {
             return View();
         }
-        /*
+
+        #endregion
+
+        #region Delete
+
+        public IActionResult Delete()
+        {
+            return View(_calculators);
+        }
+
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete()
+        public IActionResult Delete(Calculator obj)
         {
-            foreach(var obj in _db.Calculator)
-            {
-                _db.Calculator.Remove(obj);
-                _db.SaveChanges();
-            }
+            _db.Calculator.Remove(obj);
+            _db.SaveChanges();
             return RedirectToAction("Index");
         }
-        */
+
+        #endregion
     }
 }
